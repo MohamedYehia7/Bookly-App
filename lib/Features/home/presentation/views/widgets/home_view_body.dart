5a77aab7f28864347c988e5528727305d113e2e9
Mohamed Yehia <elkhamry529@gmail.com>
2023-12-04 16:05:32 +0200

    intial commit

---
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,26 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'custom_app_bar.dart';
+import 'featured_list_view.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 50,),
+          Text('Best Seller',style: Styles.titleMedium),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/widgets/splash_view_body.dart
@@ -0,0 +1,67 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/components/constants.dart';
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:bookly_app/views/widgets/sliding_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Image(
+          image: AssetImage(AssetsData.logo),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation)
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomeView()));
+    });
+  }
+}
