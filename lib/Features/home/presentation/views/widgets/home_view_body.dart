6beb4330ac910bb97998818d08240a507f7b5186
Mohamed Yehia <elkhamry529@gmail.com>
2024-04-06 10:39:37 +0200

    best seller list view item

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/assets_data.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
 
@@ -16,9 +17,39 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBooksListView(),
-          SizedBox(height: 50,),
-          Text('Best Seller',style: Styles.titleMedium),
+          SizedBox(
+            height: 50,
+          ),
+          Text('Best Seller', style: Styles.titleMedium),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                      image: AssetImage(AssetsData.testImage), fit: BoxFit.fill)),
+            ),
+          ),
+          const Column(
+            children: [],
+          )
         ],
       ),
     );
