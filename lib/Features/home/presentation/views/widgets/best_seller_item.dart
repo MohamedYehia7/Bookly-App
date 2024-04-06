32347abd43b6cb73361b2e599589345425265f5f
Mohamed Yehia <elkhamry529@gmail.com>
2024-04-06 12:25:32 +0200

    best seller item done

--- lib/Features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../../../../components/constants.dart';
 import '../../../../../core/utils/assets_data.dart';
 import '../../../../../core/utils/styles.dart';
+import 'book_rating.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
+
 //text
   @override
   Widget build(BuildContext context) {
@@ -28,36 +31,46 @@ class BestSellerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    style:
-                    Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  )),
-              const SizedBox(
-                height: 3,
-              ),
-              const Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 3,
-              ),
-              Text(
-                '19.99 €',
-                style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ],
-          )
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      style:
+                          Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    )),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    const BookRating()
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
---
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        const Icon(FontAwesomeIcons.solidStar, color: Color(0xffFFDD4F),size: 15,),
+        const SizedBox(
+          width: 6.3,
+        ),
+        const Text(
+          '4.8',
+          style: Styles.textStyle16,
+        ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          '(2390)',
+          style: Styles.textStyle14.copyWith(color: Colors.grey),
+        )
+      ],
+    );
+  }
+}
