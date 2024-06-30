17d63e90d78088264f3c27ee0768b31add160cc8
Mohamed Yehia <elkhamry529@gmail.com>
2024-06-30 14:53:18 +0300

    entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String author;
+  final num rate;
+  final num price;
+
+  BookEntity({required this.image, required this.title, required this.author, required this.rate, required this.price});
+}
