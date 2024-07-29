9827bef6da51584a6dd771628ceb607c66b4dde6
Mohamed Yehia <elkhamry529@gmail.com>
2024-07-29 20:09:26 +0300

    create simple bloc observer

--- lib/Features/home/presentation/views/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/views/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 import '../../../../domain/entities/book_entity.dart';
 
