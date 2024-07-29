f9363a97c45defcf96f95ba9b760399cfeebdaad
Mohamed Yehia <elkhamry529@gmail.com>
2024-07-29 18:05:24 +0300

    implement newest books cubit

---
+++ lib/Features/home/presentation/views/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import '../../../../domain/entities/book_entity.dart';
+import '../../../../domain/use_cases/fetch_newest_books_use_case.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.newestBooksUseCase) : super(NewestBooksInitial());
+
+  final FetchNewestBooksUseCase newestBooksUseCase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await newestBooksUseCase.call();
+    result.fold((l) {
+      emit(NewestBooksFailed(l.message));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
---
+++ lib/Features/home/presentation/views/manager/newest_books_cubit/newest_books_state.dart
@@ -0,0 +1,20 @@
+part of 'newest_books_cubit.dart';
+
+@immutable
+sealed class NewestBooksState {}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksFailed extends NewestBooksState {
+  final String message;
+
+  NewestBooksFailed(this.message);
+}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  NewestBooksSuccess(this.books);
+}
